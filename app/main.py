4df998c39d58deb8a85c65f5dd6c9c609169b6f0
orbad <orbadani6@gmail.com>
2024-09-19 22:40:19 +0300

    Added update endpoint, working locally without a DB

--- app/main.py
+++ app/main.py
@@ -9,43 +9,55 @@ class Contact(BaseModel):
     last_name: str
     phone_number: str
     address: str
-    id: int
 
 contacts = {
     "0544605039": Contact(first_name="Or", last_name="Test1", phone_number="0544605039", address="Shmorack 18, Holon", id=0),
     "0525745672": Contact(first_name="Or", last_name="Test2", phone_number="0525745672", address="Igal Alon 20, Tel Aviv", id=1),
-    2: Contact(first_name="Omri", last_name="Kash", phone_number="054630003", address="Petach Tikva", id=2)
+    "054630003": Contact(first_name="Omri", last_name="Kash", phone_number="054630003", address="Petach Tikva", id=2)
 }
 
 @app.get("/")
-def index() -> dict[str, dict[int, Contact]]:
+def index() -> dict[str, dict[str, Contact]]:
     return {"contacts": contacts}
-
-# @app.get("/phonebook/search/{item_id}")
-# def query_contact_by_phone_number(item_id: int) -> Contact:
-#     try:
-#         if item_id not in contacts:
-#             raise HTTPException(status_code=404, detail=f"The contact with id {item_id} is not found.")
-#         return contacts[item_id]
-#     finally:
-#         print("Hi")
 
 @app.get("/phonebook/search/{phone}")
 def query_contact_by_phone_number(phone: str) -> Contact:
-    try:
-        if phone not in contacts:
-            raise HTTPException(status_code=404, detail=f"The contact with phone number {phone} is not found.")
-        return contacts[phone]
-    finally:
-        print("Hi")
+    if phone not in contacts:
+        raise HTTPException(status_code=404, detail=f"The contact with phone number {phone} is not found.")
+    return contacts[phone]
+
 
 @app.post("/")
 def add_contact(contact: Contact) -> dict[str, Contact]:
-    if contact in contacts:
+    if contact.phone_number in contacts:
         HTTPException(status_code=400, detail=f"A contact with the phone number {contact.phone_number} already exists.")
 
     contacts[contact.phone_number] = contact
     return {"added": contact}
+
+@app.put("/update/{phone_number}")
+def update(
+        phone_number: str,
+        first_name: str | None = None,
+        last_name: str | None = None,
+        address: str | None =None
+) -> dict[str, Contact]:
+    if phone_number not in contacts:
+        HTTPException(status_code=404, detail=f"Contact with {phone_number=} does not exist.")
+    if all(info is None for info in (first_name, last_name, address)):
+        raise HTTPException(
+            status_code=400, detail=f"No parameters provided for update."
+         )
+    contact = contacts[phone_number]
+    if first_name is not None:
+        contact.first_name = first_name
+    if last_name is not None:
+        contact.last_name = last_name
+    if address is not None:
+        contact.address = address
+
+    return {"Updated": contact}
+
 
 @app.delete("/delete/{phone_number}")
 def delete_contact(phone_number: str) -> dict[str, Contact]:
