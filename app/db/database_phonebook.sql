9405a67e290da926f2dbd9ef6910c7bd64357aa5
orbad <orbadani6@gmail.com>
2024-09-21 18:27:06 +0300

    Connected MySQL to API, separating models, schemas and crud actions for better readability. Changed the phone_number len to be from 1 digit to 14 digits.

--- app/db/database_phonebook.sql
+++ app/db/database_phonebook.sql
@@ -2,13 +2,10 @@ CREATE DATABASE IF NOT EXISTS phonebook_db;
 USE phonebook_db;
 
 CREATE TABLE contacts (
-
                           id INT AUTO_INCREMENT PRIMARY KEY,
-                          phone_number CHAR(10) NOT NULL UNIQUE,
+                          phone_number CHAR(14) NOT NULL UNIQUE,
                           first_name VARCHAR(255) NOT NULL,
                           last_name VARCHAR(255) NOT NULL,
                           address VARCHAR(255),
-                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                          updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                          CHECK (phone_number REGEXP '^[0-9]{10}$')
+                          CHECK (phone_number REGEXP '^[0-9]{1,14}$')
     );
--- app/main.py
+++ app/main.py
@@ -1,71 +1,84 @@
-from enum import Enum
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, Depends, HTTPException
+from pydantic import ValidationError
+from sqlalchemy.orm import Session
+from db.session import Base
+from schemas import schemas
+from db.session import SessionLocal, engine
+from crud import contact as crud_contact
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+from typing import Optional
 
+logger.debug("Starting FastAPI app...")
+Base.metadata.create_all(bind=engine)
+logger.debug("Database setup completed.")
 app = FastAPI()
 
-class Contact(BaseModel):
-    first_name: str
-    last_name: str
-    phone_number: str
-    address: str
+# Dependency to get the database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-contacts = {
-    "0544605039": Contact(first_name="Or", last_name="Test1", phone_number="0544605039", address="Shmorack 18, Holon", id=0),
-    "0525745672": Contact(first_name="Or", last_name="Test2", phone_number="0525745672", address="Igal Alon 20, Tel Aviv", id=1),
-    "054630003": Contact(first_name="Omri", last_name="Kash", phone_number="054630003", address="Petach Tikva", id=2)
-}
-
-@app.get("/")
-def index() -> dict[str, dict[str, Contact]]:
-    return {"contacts": contacts}
-
-@app.get("/phonebook/search/{phone}")
-def query_contact_by_phone_number(phone: str) -> Contact:
-    if phone not in contacts:
-        raise HTTPException(status_code=404, detail=f"The contact with phone number {phone} is not found.")
-    return contacts[phone]
-
-
-@app.post("/")
-def add_contact(contact: Contact) -> dict[str, Contact]:
-    if contact.phone_number in contacts:
-        HTTPException(status_code=400, detail=f"A contact with the phone number {contact.phone_number} already exists.")
-
-    contacts[contact.phone_number] = contact
-    return {"added": contact}
-
-@app.put("/update/{phone_number}")
-def update(
-        phone_number: str,
-        first_name: str | None = None,
-        last_name: str | None = None,
-        address: str | None =None
-) -> dict[str, Contact]:
-    if phone_number not in contacts:
-        HTTPException(status_code=404, detail=f"Contact with {phone_number=} does not exist.")
-    if all(info is None for info in (first_name, last_name, address)):
+# Create a contact
+@app.post("/contacts/", response_model=schemas.Contact)
+def create_contact(contact: schemas.ContactCreate, db: Session = Depends(get_db)):
+    existing_contact = crud_contact.search_contacts(db, phone_number=contact.phone_number)
+    if existing_contact:
         raise HTTPException(
-            status_code=400, detail=f"No parameters provided for update."
-         )
-    contact = contacts[phone_number]
-    if first_name is not None:
-        contact.first_name = first_name
-    if last_name is not None:
-        contact.last_name = last_name
-    if address is not None:
-        contact.address = address
-
-    return {"Updated": contact}
-
-
-@app.delete("/delete/{phone_number}")
-def delete_contact(phone_number: str) -> dict[str, Contact]:
-
-    if phone_number not in contacts:
-        raise HTTPException(
-            status_code=404, detail=f"Contact with {phone_number=} does not exist."
+            status_code=400,
+            detail=f"A contact with the phone number {contact.phone_number} already exists."
         )
-    contact = contacts.pop(phone_number)
-    return {"Deleted contact": contact}
+    try:
+        db_contact = crud_contact.create_contact(db=db, contact=contact)
+    except ValidationError as e:
+        raise HTTPException(status_code=422, detail="Invalid phone number: must be between 1 and 14 digits.")
+    return db_contact
 
+# Get a contact by ID
+@app.get("/contacts/search", response_model=list[schemas.Contact])
+def search_contacts(
+        phone_number: Optional[str] = None,
+        first_name: Optional[str] = None,
+        last_name: Optional[str] = None,
+        address: Optional[str] = None,
+        db: Session = Depends(get_db)
+):
+    contacts = crud_contact.search_contacts(db=db, phone_number=phone_number, first_name=first_name, last_name=last_name, address=address)
+    if not contacts:
+        raise HTTPException(status_code=404, detail="No contacts found")
+
+    return contacts
+
+# Get all contacts
+@app.get("/contacts/", response_model=list[schemas.Contact])
+def read_contacts(offset: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    contacts = crud_contact.get_contacts(db=db, offset=offset, limit=limit)
+    return contacts
+
+# Update a contact by ID
+@app.put("/contacts/{phone_number}", response_model=schemas.Contact)
+def update_contact(phone_number: str, contact: schemas.ContactUpdate, db: Session = Depends(get_db)):
+    db_contact = crud_contact.search_contacts(db, phone_number=phone_number)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+
+    if all(
+            value is None for value in (contact.first_name, contact.last_name, contact.address)
+    ):
+        raise HTTPException(
+            status_code=400, detail="No parameters provided for update."
+        )
+    updated_contact = crud_contact.update_contact(db=db, phone_number=phone_number, contact_update=contact)
+    return updated_contact
+
+# Delete a contact by ID
+@app.delete("/contacts/{phone_number}", response_model=schemas.Contact)
+def delete_contact(phone_number: str, db: Session = Depends(get_db)):
+    db_contact = crud_contact.delete_contact(db=db, phone_number=phone_number)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return db_contact
