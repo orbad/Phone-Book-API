adca375f4b13d938a35e7d24cc1eb527f2e3e787
orbad <orbadani6@gmail.com>
2024-09-21 18:54:41 +0300

    Removed redundant models and schemas python files, Added requirements for fetching enviornment variables from .env

--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
 import os
 
+load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
